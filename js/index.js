a2c232ade09273d70b2462e91b3bbe7ad3221197
maplestartend <asercv14632@gmail.com>
2023-11-24 01:24:10 +0800

    新增首頁手機版banner及js切換顯示

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+const banner1 = document.getElementById("banner1");
+const banner2 = document.getElementById("banner2");
+const banner3 = document.getElementById("banner3");
 const qaTabAll = document.querySelectorAll(".i-qa-tab");
 const qaTab = document.querySelector(".i-qa-tab");
 const qaImg = document.getElementById("qaImg");
@@ -6,7 +9,33 @@ iconImg.setAttribute("src", "img/yarn.svg");
 
 window.onload = function () {
   qaTab.classList.add("i-qa-tab_active"), qaTab.appendChild(iconImg);
+  resizeBanner();
 };
+
+window.addEventListener("resize", (e) => {
+  // documentWidth();
+  resizeBanner();
+});
+// function documentWidth() {
+//   const doc = document.documentElement;
+//   console.log(doc);
+//   doc.style.setProperty("--doc-width", `${window.innerWidth}px`);
+// }
+// documentWidth();
+
+//reset while resize
+
+function resizeBanner() {
+  if (window.innerWidth < 500) {
+    banner1.setAttribute("src", "img/banner1-sm.png");
+    banner2.setAttribute("src", "img/banner2-sm.png");
+    banner3.setAttribute("src", "img/banner3-sm.png");
+  } else {
+    banner1.setAttribute("src", "img/banner1.png");
+    banner2.setAttribute("src", "img/banner2.png");
+    banner3.setAttribute("src", "img/banner3.png");
+  }
+}
 
 qaTabAll.forEach((element) => {
   element.addEventListener("click", function () {
