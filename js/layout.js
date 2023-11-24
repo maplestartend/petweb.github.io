e0b87a235d6146941185bd0214a088819f3200db
maplestartend <asercv14632@gmail.com>
2023-11-24 18:00:56 +0800

    header漢堡選單測試

--- js/layout.js
+++ js/layout.js
@@ -10,3 +10,30 @@ headerLink.forEach((element) => {
     element.removeChild(headerIconimg);
   });
 });
+
+const headerNav = document.querySelector(".header-hamburger");
+const navBg = document.querySelector(".nav-bg");
+
+headerNav.addEventListener("click", function () {
+  navBg.classList.toggle("nav-bg_active");
+});
+
+const sectionHeroEl = document.querySelector(".swiper");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (!ent.isIntersecting) document.body.classList.add("sticky");
+
+    if (ent.isIntersecting) document.body.classList.remove("sticky");
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-100px",
+  }
+);
+obs.observe(sectionHeroEl);
